847d4432fef0feb5739d62d23ff35f1af047d6e8
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-06-10 11:31:52 -0500

    🎨 ui: Design the navbar and hero section for the landing page

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/shared/components/landing/Hero";
+import Navbar from "@/shared/components/landing/NavBar";
+
+export const Landing = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
+};
