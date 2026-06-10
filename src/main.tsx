2f3ed91ba11e24bbdd711e4fae246e634ea98f79
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-06-10 10:35:42 -0500

    📦️ packages: Install tailwind and shadcn and configure it

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./styles/index.css";
 
-createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>Hola</StrictMode>,
+);
