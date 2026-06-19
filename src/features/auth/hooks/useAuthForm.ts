54a3169735d21ea66e88791b3acbf1cbfb1ae1a6
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-06-19 16:20:51 -0500

    📦 packages: Install zod

--- src/features/auth/hooks/useAuthForm.ts
+++ src/features/auth/hooks/useAuthForm.ts
@@ -1,25 +1,21 @@
-import { useRef, useState, type FocusEvent, type SubmitEvent } from "react";
-import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
+import { useRef, useState, type FocusEvent, type SubmitEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
-import {
-  isInvalidEmail,
-  isInvalidPassword,
-  isInvalidUsername,
-} from "../utils/form.helper";
-import { createBaseNewUser } from "../utils/firebase.helper";
+import { isInvalidEmail, isInvalidPassword, isInvalidUsername } from '../utils/form.helper';
+import { createBaseNewUser } from '../utils/firebase.helper';
 
-import type { UserRegister } from "../types/user.type";
-import { useAuth } from "./useAuth";
+import type { UserRegister } from '../types/user.type';
+import { useAuth } from './useAuth';
 
-type AuthFields = "email" | "username" | "password";
+type AuthFields = 'email' | 'username' | 'password';
 
 export const useAuthForm = (isSignup: boolean) => {
   //* States
   const [formErrors, setFormErrors] = useState<Record<AuthFields, string>>({
-    email: "",
-    username: "",
-    password: "",
+    email: '',
+    username: '',
+    password: '',
   });
 
   //* Contexts
@@ -34,56 +30,49 @@ export const useAuthForm = (isSignup: boolean) => {
   //* Handlers
 
   // ? Blur validations
-  const handleBlurEmail = (
-    e: FocusEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
+  const handleBlurEmail = (e: FocusEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     if (isInvalidEmail(e.target.value)) {
       setFormErrors((prev) => ({
         ...prev,
-        email: "Please enter a valid email address.",
+        email: 'Please enter a valid email address.',
       }));
       return;
     }
 
     setFormErrors((prev) => ({
       ...prev,
-      email: "",
+      email: '',
     }));
   };
 
-  const handleBlurUsername = (
-    e: FocusEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
+  const handleBlurUsername = (e: FocusEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     if (isInvalidUsername(e.target.value)) {
       setFormErrors((prev) => ({
         ...prev,
-        username:
-          "Username must be 3–20 characters long and contain only lowercase letters, numbers, and underscores.",
+        username: 'Username must be 3–20 characters long and contain only lowercase letters, numbers, and underscores.',
       }));
       return;
     }
 
     setFormErrors((prev) => ({
       ...prev,
-      username: "",
+      username: '',
     }));
   };
 
-  const handleBlurPassword = (
-    e: FocusEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
+  const handleBlurPassword = (e: FocusEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     if (isInvalidPassword(e.target.value)) {
       setFormErrors((prev) => ({
         ...prev,
         password:
-          "Password must be at least 8 characters long and include an uppercase letter, a number, and a special character.",
+          'Password must be at least 8 characters long and include an uppercase letter, a number, and a special character.',
       }));
       return;
     }
 
     setFormErrors((prev) => ({
       ...prev,
-      password: "",
+      password: '',
     }));
   };
 
@@ -93,31 +82,23 @@ export const useAuthForm = (isSignup: boolean) => {
     if (!formRef.current) return;
 
     // Data validation
-    const { email, username, password } = Object.fromEntries(
-      new FormData(formRef.current),
-    ) as UserRegister;
+    const { email, username, password } = Object.fromEntries(new FormData(formRef.current)) as UserRegister;
 
-    if (
-      isInvalidEmail(email) ||
-      isInvalidUsername(username) ||
-      isInvalidPassword(password)
-    ) {
-      toast.error("There are invalid fields");
+    if (isInvalidEmail(email) || isInvalidUsername(username) || isInvalidPassword(password)) {
+      toast.error('There are invalid fields');
       return;
     }
 
     // Register the new base user
-    const errorMessage = await registerWithEmailAndPassword(
-      createBaseNewUser(email, username, password),
-    );
+    const errorMessage = await registerWithEmailAndPassword(createBaseNewUser(email, username, password));
 
     if (errorMessage) {
       toast.error(errorMessage);
       return;
     }
 
-    toast.success("User created successfully");
-    navigate("/profile", { replace: true });
+    toast.success('User created successfully');
+    navigate('/profile', { replace: true });
   };
 
   return {
