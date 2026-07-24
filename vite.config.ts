bcd9209d412a2cda5c0d86518166584dda8fd61f
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-07-24 13:47:51 -0500

    📦 packages: Add the rollup-plugin-visualizer to measure the size of the build bundle

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
+import { visualizer } from "rollup-plugin-visualizer";
 import path from "path";
 
 // https://vite.dev/config/
@@ -10,6 +11,10 @@ export default defineConfig({
     react(),
     tailwindcss(),
     babel({ presets: [reactCompilerPreset()] }),
+    visualizer({
+      open: true,
+      gzipSize: true,
+    }),
   ],
   resolve: {
     alias: {
