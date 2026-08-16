1fc5ed0a2de8ba6364ab5c1c81b15795bffbce24
Eddie <eddiesantiago122007@gmail.com>
2026-08-16 17:23:22 -0500

    ✨ feat: Configure Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import { BrowserRouter } from "react-router-dom";
 import { LinknestApp } from "./LinknestApp";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Analytics />
+
     <BrowserRouter>
       <LinknestApp />
     </BrowserRouter>
